3fc7e4782d722e4f879d378768da99779cf09b66
maglovskiNenad <maglovskin@gmail.com>
2024-09-24 22:11:34 +0200

    image

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 /*Toggle between showing and hiding the navigation menu links */
-function toggleMe() {}
+function openNav() {
+  let navbar = document.getElementById("navbar");
+  navbar.style.display = navbar.style.display === "none" ? "block" : "none";
+}
