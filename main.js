1ca9a055de1b03fafb2b29e29f39d4119f7f5be6
maglovskiNenad <maglovskin@gmail.com>
2024-10-01 23:17:35 +0200

    add

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-/*Toggle between showing and hiding the navigation menu links */
-function openNav() {
-  let navbar = document.getElementById("navbar");
-  navbar.style.display = navbar.style.display === "none" ? "block" : "none";
-}
+let navbar = document.getElementById("navbar");
 
-function toggleMode() {
-  var element = document.body;
+header.addEventListener("click", function () {
+  navbar.style.display = navbar.style.display === "none" ? "block" : "none";
+});
+
+let toggle = document.getElementById("toggleBtn");
+
+toggle.addEventListener("click", function () {
   element.classList.toggle("dark-mode");
-}
+});
+
+let header = document.getElementById("header");
+var element = document.body;
+
+window.addEventListener("resize", function () {
+  let width = this.window.outerWidth;
+  if (width > 600) {
+    navbar.style.display = "flex";
+  } else {
+    navbar.style.display = "block";
+  }
+});
