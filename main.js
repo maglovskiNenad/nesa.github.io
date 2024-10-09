14f0e0d0e70a7115a06d6ac07c4d0ab25ca14d5b
Maglovski Nenad <79421496+maglovskiNenad@users.noreply.github.com>
2024-10-09 22:53:57 +0200

    larger screen

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-/*let header = document.getElementById("header");
-let visibleNavbar = document.getElementById("visible--navbar");
-
-//On resize acting on navbar to change from dropdown menu to navbar menu
-window.addEventListener("resize", function () {
-  //Checkinh number of pixels
-  if (this.document.documentElement.clientWidth < 600) {
-    visibleNavbar.style.display = "none";
-  } else {
-    visibleNavbar.style.display = "flex";
-  }
-});
-
-//Animation on burger menu
-header.addEventListener("click", function () {
-  //Changing the visabiliti for dropdown menu
-  visibleNavbar.classList.toggle("navbar--menu");
-  //Adding or removing special aniamtion class list for burger menu
-  document.querySelector(".one").classList.toggle("one--animation");
-  document.querySelector(".two").classList.toggle("two--animation");
-  document.querySelector(".three").classList.toggle("three--animation");
-});
-*/
-
 let navbarSectionUl = document.getElementById("ul--section--navbar");
 let visibleNavbar = document.getElementById("visible--navbar");
 let closeNavbarBtn = document.getElementById("close--navbar--btn");
@@ -33,4 +9,12 @@ navbarSectionUl.addEventListener("click", function () {
 
 closeNavbarBtn.addEventListener("click", function () {
   visibleNavbar.style.display = "none";
+});
+
+window.addEventListener("resize", function () {
+  if (this.document.documentElement.clientWidth >= 601) {
+    visibleNavbar.style.display = "flex";
+  } else {
+    visibleNavbar.style.display = "none";
+  }
 });
